2c0a1f6b728cc12900978d400e387525466ff22e
jcvar <61365224+jcvar@users.noreply.github.com>
2020-04-03 15:49:40 -0500

    Init spring boot app.

---
+++ src/main/java/agricolab/app/AgriColabAppApplication.java
@@ -0,0 +1,13 @@
+package agricolab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriColabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriColabAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agricolab/app/AgriColabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package agricolab.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriColabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
