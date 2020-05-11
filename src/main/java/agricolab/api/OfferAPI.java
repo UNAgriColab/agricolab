30c95ddfeb2396cc12694518852d719dc8b2238c
saduquebe <santo0103@live.com>
2020-05-11 18:36:57 -0500

    JWT Functional Changes

--- src/main/java/agricolab/api/OfferAPI.java
+++ src/main/java/agricolab/api/OfferAPI.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping(value = "/api/v1/offer")
 public class OfferAPI {
