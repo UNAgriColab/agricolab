95679bab7b19967735dc3ef8e0357082138888a3
saduquebe <saduquebe@unal.edu.co>
2020-06-11 19:40:19 -0500

    PaginationChanges

--- src/main/java/agricolab/model/Offer.java
+++ src/main/java/agricolab/model/Offer.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Offer {
 
-    private String id;
+    private int id;
     private String sellerEmail;
     private String productName;
     private int presentation;
@@ -67,11 +67,11 @@ public class Offer {
                 '}';
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
