c5758968097eed6bf920cb039feb4bf26c525e46
jcvar <36166613+jcvar@users.noreply.github.com>
2020-06-14 22:36:38 -0500

    Basic photo upload

--- src/main/java/agricolab/dao/OrderFirestoreDAO.java
+++ src/main/java/agricolab/dao/OrderFirestoreDAO.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
-@Repository
+@Repository("Firestore")
 public class OrderFirestoreDAO implements OrderDAO {
 
 
